f09279553545ce0d3a5f2089d274b207e95d0870
“chrisshank” <chris.shank.23@gmail.com>
2024-02-13 13:31:41 -0800

    scaffold core compiler functions

--- src/compiler.c
+++ src/compiler.c
@@ -1,6 +1,32 @@
 #include "emscripten.h"
 
 EMSCRIPTEN_KEEPALIVE
-int sum(int a, int b) {
-  return a + b;
+int lex() {
+  return 1;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int tokenize() {
+  return 2;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int parse() {
+  return 3;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int process() {
+  return 4;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int analyze() {
+  return 5;
+}
+
+// Do we need to link in here?
+EMSCRIPTEN_KEEPALIVE
+int link() {
+  return 6;
 }
